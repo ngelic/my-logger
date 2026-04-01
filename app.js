d087ef5a91c9b0be5c65559a0219002607aa0414
angel <1to10petals@gmail.com>
2026-04-01 12:14:50 +0400

    removed text and added pacman redirect

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Request Logged!</h1>');
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
